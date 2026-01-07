f8548a28ee53cda5868116dbe59186f62ab3d4ae
ocavue <ocavue@gmail.com>
2026-01-07 23:46:24 +1100

    add svelte

---
+++ src/components/apple.svelte
@@ -0,0 +1,17 @@
+<script>
+    let apple = $state({
+        color: 'red',
+    });
+</script>
+
+<div class="apple">
+    {apple.color}
+</div>
+
+<style>
+  .apple {
+    width: 100px;
+    height: 100px;
+    background-color: red;
+  }
+</style>
